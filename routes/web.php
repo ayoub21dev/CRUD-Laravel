5c22eb0de3b0d5f62c55fdbe784c83009582537e
ayoub21dev <Ayoubcode941@gmail.com>
2025-10-14 15:45:21 +0100

    feat: Implement MVC structure for pages and articles

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\PageController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -6,4 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
  
-Route ::get('/ayoub',fn()=>'ayoub');
+Route ::get('/ping',fn()=>'pong');
+
+Route ::get('/',[PageController::class, 'home'])->name('home');
+Route ::get('/a-propos',[PageController::Class,'about'] )->name('about');
+
+
+// Mini-routes articles (mockées pour l’instant)
+Route::get('/articles', [PageController::class, 'articles'])->name('articles.index');
+Route::get('/articles/{slug}', [PageController::class, 'show'])->name('articles.show');
