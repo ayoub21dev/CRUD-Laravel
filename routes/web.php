679180bcb62d00583c982d5283f64e0e7796773f
ayoub21dev <Ayoubcode941@gmail.com>
2025-12-04 10:02:09 +0000

    add fn is_admin

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/admin', function () {return view('admin.dashboard');})->middleware('auth')->name('admin.dashboard');
+
